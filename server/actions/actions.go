550d67060d87d632f1542138ec4566c5a0f7750c
priyansh-garg1 <priyanshgarg382@gmail.com>
2024-10-23 00:40:53 +0530

    Added LIKE DISLIKE feature

--- server/actions/actions.go
+++ server/actions/actions.go
@@ -4,113 +4,167 @@ import (
 	"context"
 	"errors"
 	"fmt"
+
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 )
 
+type Database struct {
+	driver neo4j.DriverWithContext
+}
+
+func NewDatabase(driver neo4j.DriverWithContext) *Database {
+	return &Database{driver: driver}
+}
+
 type User struct {
-    ApplicationId string `json:"application_id"`
-    Name          string `json:"name"`
-    Email         string `json:"email"`
+	ApplicationId string `json:"application_id"`
+	Name          string `json:"name"`
+	Email         string `json:"email"`
 }
 
-func FetchUserByApplicationId(driver neo4j.DriverWithContext, applicationId string) (*User, error) {
+func (db *Database) FetchUserByApplicationId(applicationId string) (*User, error) {
+	session := db.driver.NewSession(context.Background(), neo4j.SessionConfig{})
+	defer session.Close(context.Background())
 
-	session := driver.NewSession(context.Background(), neo4j.SessionConfig{})
-    defer session.Close(context.Background())
+	query := `
+		MATCH (u:User {applicationId: $applicationId})
+		RETURN u`
+	parameters := map[string]interface{}{
+		"applicationId": applicationId,
+	}
 
-    query := `
-        MATCH (u:User {applicationId: $applicationId})
-        RETURN u`
-    parameters := map[string]interface{}{
-        "applicationId": applicationId,
-    }
+	result, err := session.Run(context.Background(), query, parameters)
+	if err != nil {
+		return nil, err
+	}
 
-    result, err := session.Run(context.Background(), query, parameters)
-    if err != nil {
-        return nil, err
-    }
+	if !result.Next(context.Background()) {
+		return nil, errors.New("user not found")
+	}
 
-    if !result.Next(context.Background()) {
-        return nil, errors.New("user not found")
-    }
+	node := result.Record().Values[0].(neo4j.Node)
 
-    node := result.Record().Values[0].(neo4j.Node)
+	user := &User{
+		Name:          fmt.Sprintf("%v", node.Props["name"]),
+		Email:         fmt.Sprintf("%v", node.Props["email"]),
+		ApplicationId: fmt.Sprintf("%v", node.Props["applicationId"]),
+	}
 
-    user := &User{
-		Name:           fmt.Sprintf("%v", node.Props["name"]),
-        Email:          fmt.Sprintf("%v", node.Props["email"]),
-        ApplicationId:  fmt.Sprintf("%v", node.Props["applicationId"]),
-    }
+	return user, nil
+}
 
-    return user, nil
+func (db *Database) InsertUser(user User) error {
+	session := db.driver.NewSession(context.Background(), neo4j.SessionConfig{})
+	defer session.Close(context.Background())
+
+	query := `
+		CREATE (u: User {name: $name, applicationId: $applicationId, email: $email})
+		RETURN u`
+	parameters := map[string]interface{}{
+		"name":          user.Name,
+		"applicationId": user.ApplicationId,
+		"email":         user.Email,
+	}
+
+	result, err := session.Run(context.Background(), query, parameters)
+	if err != nil {
+		return err
+	}
+
+	if !result.Next(context.Background()) {
+		return errors.New("failed to insert user")
+	}
+
+	return nil
+}
+
+func (db *Database) GetUsersWithNoConnection(applicationId string) ([]User, error) {
+	session := db.driver.NewSession(context.Background(), neo4j.SessionConfig{})
+	defer session.Close(context.Background())
+
+	query := `
+		MATCH (cu: User {applicationId: $applicationId}) 
+		MATCH (ou: User) 
+		WHERE NOT (cu)-[:LIKE|:DILIKE]->(ou) AND cu <> ou 
+		RETURN ou`
+	parameters := map[string]interface{}{
+		"applicationId": applicationId,
+	}
+
+	result, err := session.Run(context.Background(), query, parameters)
+	if err != nil {
+		return nil, fmt.Errorf("error executing query: %v", err)
+	}
+
+	var users []User
+	for result.Next(context.Background()) {
+		node := result.Record().Values[0].(neo4j.Node)
+
+		user := User{
+			Email:         fmt.Sprintf("%v", node.Props["email"]),
+			Name:          fmt.Sprintf("%v", node.Props["name"]),
+			ApplicationId: fmt.Sprintf("%v", node.Props["applicationId"]),
+		}
+
+		users = append(users, user)
+	}
+
+	if err := result.Err(); err != nil {
+		return nil, fmt.Errorf("error processing query result: %v", err)
+	}
+
+	return users, nil
 }
 
 
-func InsertUser(driver neo4j.DriverWithContext, user User) error {
-    
+func (db *Database) Neo4jSwipe(id string,swipe string,userId string) (bool, error) {
+	var swipeType string
+	if swipe == "left" {
+		swipeType = "DISLIKE"
+	} else {
+		swipeType = "LIKE"
+	} 
 
-    session := driver.NewSession(context.Background(), neo4j.SessionConfig{})
-    defer session.Close(context.Background())
+	session := db.driver.NewSession(context.Background(), neo4j.SessionConfig{})
+	defer session.Close(context.Background())
 
-    query := `
-        CREATE (u: User {name: $name, applicationId: $applicationId, email: $email})
-        RETURN u`
-    parameters := map[string]interface{}{
-        "name":          user.Name,
-        "applicationId": user.ApplicationId,
-        "email":         user.Email,
-    }
+	query := fmt.Sprintf(`
+        MATCH (cu: User { applicationId: $id }), (ou: User { applicationId: $userId })
+        CREATE (cu)-[:%s]->(ou)`, swipeType)
 
-    result, err := session.Run(context.Background(), query, parameters)
-    if err != nil {
-        return err
-    }
+	parameters := map[string]interface{}{
+		"id":     id,
+		"userId": userId,
+	}
 
-    if !result.Next(context.Background()) {
-        return errors.New("failed to insert user")
-    }
+	_, err := session.Run(context.Background(), query, parameters)
+	if err != nil {
+		return false, fmt.Errorf("error executing query: %v", err)
+	}
 
-    return nil
-}
+	if swipeType == "LIKE" {
+		query := `MATCH (cu: User{applicationId: $id}), (ou: User{applicationId: $userId}) WHERE (ou)-[:LIKE]->(cu) RETURN ou as MATCH`
+		parameters := map[string]interface{}{
+			"id":     id,
+			"userId": userId,
+		}
+	
+		result, err := session.Run(context.Background(), query, parameters)
+		if err != nil {
+			return false, fmt.Errorf("error executing query: %v", err)
+		}
+
+		fmt.Println(result)
+
+		if result.Next(context.Background()) {
+			return true, nil 
+		}
+	}
+
+	return false, nil
+
+	// fmt.Sprintf("Successfully created a %s relationship between %s and %s", swipeType, id, userId), nil
 
 
-func GetUsersWithNoConnection(driver neo4j.DriverWithContext, applicationId string) ([]User,error) {
-
-    session := driver.NewSession(context.Background(), neo4j.SessionConfig{})
-    defer session.Close(context.Background())
-
-    query := `
-        MATCH (cu: User {applicationId: $applicationId}) MATCH (ou: User) WHERE NOT (cu)-[:LIKE|:DILIKE]->(ou) AND cu <> ou RETURN ou`
-    parameters := map[string]interface{}{
-        "applicationId": applicationId,
-    }
-
-    result, err := session.Run(context.Background(), query, parameters)
-    if err != nil {
-        fmt.Println("Error executing Get user query with no connection")
-    }
-
-    if err != nil {
-        return nil, fmt.Errorf("error executing query: %v", err)
-    }
-
-    var users []User
-    for result.Next(context.Background()) {
-
-        node := result.Record().Values[0].(neo4j.Node)
-
-        user := User{
-            Email:         fmt.Sprintf("%v", node.Props["email"]),
-            Name:          fmt.Sprintf("%v", node.Props["name"]),
-            ApplicationId: fmt.Sprintf("%v", node.Props["applicationId"]),
-        }
-
-        users = append(users, user)
-    }
-
-    if err := result.Err(); err != nil {
-        return nil, fmt.Errorf("error processing query result: %v", err)
-    }
-
-    return users, nil
+	
 }
